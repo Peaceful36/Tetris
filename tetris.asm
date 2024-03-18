3e05359ea782f2940014e2d08f27537c4e48b838
Hoa.36 <54192376+Peaceful36@users.noreply.github.com>
2024-03-18 13:49:17 -0400

    Add files via upload

--- tetris.asm
+++ tetris.asm
@@ -4,10 +4,10 @@
 # Student 1: Hoa Nguyen, 1007996830
 # Student 2: Name, Student Number (if applicable)
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels:       20
-# - Unit height in pixels:      20
-# - Display width in pixels:    1
-# - Display height in pixels:   1
+# - Unit width in pixels:       8 
+# - Unit height in pixels:      8
+# - Display width in pixels:    256
+# - Display height in pixels:   256
 # - Base Address for Display:   0x10008000 ($gp)
 ##############################################################################
 
@@ -25,7 +25,16 @@ ADDR_KBRD:
 ##############################################################################
 # Mutable Data
 ##############################################################################
+GREY: .word 0xaaaaaa
 
+#Walls
+WALLS:
+.word 0x19010500
+.word 0
+.word 0x1901051f
+.word 0
+.word 0x011e0501
+.word 0
 ##############################################################################
 # Code
 ##############################################################################
@@ -35,6 +44,39 @@ ADDR_KBRD:
 	# Run the Tetris game.
 main:
     # Initialize the game
+    # initialize the loop variables $t5, $t6
+add $t5, $zero, $zero	# set $t5 to zero
+addi $t6, $zero, 32
+lw $t1, GREY
+lw $t0, ADDR_DSPL
+
+draw_walls_sides:
+beq $t5, $t6, reset_value	# Branch to Exit if $t5 == 16
+
+sw $t1, 0($t0) 		# paint the first (top-left) unit
+addi $t0, $t0, 80 	# move to the next pixel over in the bitmap
+sw $t1, 0($t0) 		# paint the first unit on the second row.
+addi $t0, $t0, 48   	
+sw $t1, 0($t0) 	
+addi $t5, $t5, 1	# increment $t5 by 1
+j draw_walls_sides
+
+reset_value: # reset the register to start drawing bottom wall
+add $t5, $zero, $zero	# set $t5 to zero
+addi $t6, $zero, 18
+lw $t0, ADDR_DSPL
+addi $t0, $t0, 3972
+j draw_wall_bottom
+
+draw_wall_bottom:
+beq $t5, $t6, Exit
+sw $t1, 0($t0)
+addi, $t0, $t0, 4
+sw $t1, 0($t0)
+addi $t5, $t5, 1	# increment $t5 by 1
+j draw_wall_bottom
+
+
 
 game_loop:
 	# 1a. Check if key has been pressed
@@ -46,3 +88,7 @@ game_loop:
 
     #5. Go back to 1
     b game_loop
+
+Exit:
+li $v0, 10 # terminate the program gracefully
+syscall
