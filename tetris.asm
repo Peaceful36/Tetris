9254fd77bba43102864d4b68f693a0730a3933a9
Hoa.36 <54192376+Peaceful36@users.noreply.github.com>
2024-03-24 19:11:43 -0400

    Milestone 1 finished

--- tetris.asm
+++ tetris.asm
@@ -26,6 +26,15 @@ ADDR_KBRD:
 # Mutable Data
 ##############################################################################
 GREY: .word 0xaaaaaa
+light_grey: .word 0xd3d3d3
+dark_grey: .word 0x3b3b3b
+RED: .word 0x44050f
+ORANGE: .word 0x4d2c07
+YELLOW: .word 0xFFFF00
+GREEN: .word 0x2a420f
+BLUE: .word 0x26404a
+PURPLE: .word 0x21192e
+
 ##############################################################################
 # Code
 ##############################################################################
@@ -37,7 +46,7 @@ main:
     # Initialize the game
     # initialize the loop variables $t5, $t6
 add $t5, $zero, $zero	# set $t5 to zero
-addi $t6, $zero, 32
+addi $t6, $zero, 20
 lw $t1, GREY
 lw $t0, ADDR_DSPL
 
@@ -45,29 +54,80 @@ draw_walls_sides:
 beq $t5, $t6, reset_value	# Branch to Exit if $t5 == 16
 
 sw $t1, 0($t0) 		# paint the first (top-left) unit
-addi $t0, $t0, 80 	# move to the next pixel over in the bitmap
+addi $t0, $t0, 44 	# move to the next pixel over in the bitmap
 sw $t1, 0($t0) 		# paint the first unit on the second row.
-addi $t0, $t0, 48   	
+addi $t0, $t0, 84   	
 sw $t1, 0($t0) 	
 addi $t5, $t5, 1	# increment $t5 by 1
 j draw_walls_sides
 
 reset_value: # reset the register to start drawing bottom wall
 add $t5, $zero, $zero	# set $t5 to zero
-addi $t6, $zero, 18
+addi $t6, $zero, 11
 lw $t0, ADDR_DSPL
-addi $t0, $t0, 3972
+addi $t0, $t0, 2560
 j draw_wall_bottom
 
 draw_wall_bottom:
-beq $t5, $t6, Exit
+beq $t5, $t6, init_draw_grid #start drawing grid after finish all the walls
 sw $t1, 0($t0)
 addi, $t0, $t0, 4
 sw $t1, 0($t0)
 addi $t5, $t5, 1	# increment $t5 by 1
 j draw_wall_bottom
 
+init_draw_grid:
+add $t5, $zero, $zero	# set $t5 to zero
+addi $t6, $zero, 5
+lw $t1, dark_grey
+lw $t0, ADDR_DSPL
+addi $t2, $zero, 19 # change this line to change grid height 
+addi $t3, $zero, 0
+add $t4, $zero, 1
+j draw_grid1
 
+to_next_line:
+add $t5, $zero, $zero	# set $t5 to zero
+beq $t2, $t3, draw_O
+addi $t0, $t0, 88
+li $t7, 2      # Load 2 into $t7
+div $t4, $t7   # Divide $t4 by 2
+mfhi $t8       # Load the remainder into $t8
+addi $t4, $t4, 1
+addi $t3, $t3, 1
+beq $t8, $zero, draw_grid1 # check which line of grid to draw
+b draw_grid2
+
+draw_grid1: # a line of the grid
+beq $t5, $t6, to_next_line
+addi $t0, $t0, 4
+sw $zero, 0($t0)
+addi $t0, $t0, 4
+sw $t1, 0($t0) 
+addi $t5, $t5, 1
+j draw_grid1
+
+draw_grid2: # another line of the grid
+beq $t5, $t6, to_next_line
+addi $t0, $t0, 4
+sw $t1, 0($t0)
+addi $t0, $t0, 4
+sw $zero, 0($t0) 
+addi $t5, $t5, 1
+j draw_grid2
+
+draw_O: # temporary code for milestone 1
+lw $t1, YELLOW
+lw $t0, ADDR_DSPL
+addi $t0, $t0, 16
+sw $t1, 0($t0)
+addi $t0, $t0, 4
+sw $t1, 0($t0)
+addi $t0, $t0, 124
+sw $t1, 0($t0)
+addi $t0, $t0, 4
+sw $t1, 0($t0)
+j Exit
 
 game_loop:
 	# 1a. Check if key has been pressed
