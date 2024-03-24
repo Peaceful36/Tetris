888778965e29c27daf2f75988ba5673cd95ca393
Hoa.36 <54192376+Peaceful36@users.noreply.github.com>
2024-03-24 19:16:01 -0400

    milestone 1

--- tetris.asm
+++ tetris.asm
@@ -7,7 +7,7 @@
 # - Unit width in pixels:       8 
 # - Unit height in pixels:      8
 # - Display width in pixels:    256
-# - Display height in pixels:   256
+# - Display height in pixels:   168
 # - Base Address for Display:   0x10008000 ($gp)
 ##############################################################################
 
