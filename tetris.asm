ea72171fa32357654131570b1854bd24da751a3f
Frank Liao <144388514+emmLiao@users.noreply.github.com>
2024-03-26 05:31:51 -0400

    Add files via upload
    randomly generate a piece

--- tetris.asm
+++ tetris.asm
@@ -2,7 +2,7 @@
 # This file contains our implementation of Tetris.
 #
 # Student 1: Hoa Nguyen, 1007996830
-# Student 2: Name, Student Number (if applicable)
+# Student 2: Shizhuo Liao 1008768442
 ######################## Bitmap Display Configuration ########################
 # - Unit width in pixels:       8 
 # - Unit height in pixels:      8
@@ -88,7 +88,7 @@ j draw_grid1
 
 to_next_line:
 add $t5, $zero, $zero	# set $t5 to zero
-beq $t2, $t3, draw_O
+beq $t2, $t3, game_loop
 addi $t0, $t0, 88
 li $t7, 2      # Load 2 into $t7
 div $t4, $t7   # Divide $t4 by 2
@@ -116,21 +116,50 @@ sw $zero, 0($t0)
 addi $t5, $t5, 1
 j draw_grid2
 
-draw_O: # temporary code for milestone 1
-lw $t1, YELLOW
-lw $t0, ADDR_DSPL
-addi $t0, $t0, 16
-sw $t1, 0($t0)
-addi $t0, $t0, 4
-sw $t1, 0($t0)
-addi $t0, $t0, 124
-sw $t1, 0($t0)
-addi $t0, $t0, 4
-sw $t1, 0($t0)
-j Exit
 
 game_loop:
+
+    li $v0, 42 #Load random integer from 0 to 6
+    li $a0, 0
+    li $a1, 6
+    syscall
+    
+    add $t7, $zero, $zero # set t7 to 0 and randomly load piece
+    beq $a0, $t7, tetris0
+    addi $t7, $t7, 1 
+    beq $a0, $t7, tetris1
+    addi $t7, $t7, 1
+    beq $a0, $t7, tetris2
+    addi $t7, $t7, 1
+    beq $a0, $t7, tetris3
+    addi $t7, $t7, 1
+    beq $a0, $t7, tetris4
+    addi $t7, $t7, 1
+    beq $a0, $t7, tetris5
+    addi $t7, $t7, 1
+    beq $a0, $t7, tetris6
+    
+    tetris0: # O tetris
+        j draw_O
+    tetris1: # S tetris
+        j draw_S
+    tetris2: # I tetris
+        j draw_I
+    tetris3: # Z tetris
+        j draw_Z
+    tetris4: # L tetris
+        j draw_L
+    tetris5: # J tetris
+        j draw_J
+    tetris6: # T tetris
+        j draw_T
+        
 	# 1a. Check if key has been pressed
+	keyboard:
+	   lw $t9, ADDR_KBRD  # Load the address of the keyboard
+	   lw $t8, 0($t9) # Load the keyboard input
+	   beq $t8, 1, keyboard_input # If first word 1, key is pressed
+	
     # 1b. Check which key has been pressed
     # 2a. Check for collisions
 	# 2b. Update locations (paddle, ball)
@@ -138,8 +167,118 @@ game_loop:
 	# 4. Sleep
 
     #5. Go back to 1
-    b game_loop
+    j keyboard
+    
+#A key is pressed
+keyboard_input:
+    lw $t7, 4($t9)
+    beq $t7, 119, wkey
+    beq $t7, 100, dkey
+    beq $t7, 115, skey
+    beq $t7, 97, akey
+    beq $t7, 113, qkey
 
+wkey:
+    
+    j keyboard
+akey:
+    
+    j keyboard
+skey:
+    
+    j keyboard
+dkey:
+
+    j keyboard
+qkey:
+    j Exit
+
+draw_O:
+    lw $t1, YELLOW
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    addi $t0, $t0, 124
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    j keyboard
+draw_S:
+    lw $t1, RED
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    addi $t0, $t0, 120
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    j keyboard
+draw_I:
+    lw $t1, BLUE
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    j keyboard
+draw_Z:
+    lw $t1, GREEN
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    j keyboard
+draw_L:
+    lw $t1, ORANGE
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    j keyboard
+draw_J:
+    lw $t1, GREY
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 20
+    sw $t1, 0($t0)
+    addi $t0, $t0, 128
+    sw $t1, 0($t0)
+    addi $t0, $t0, 124
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    j game_loop
+    
+draw_T:
+    lw $t1, PURPLE
+    lw $t0, ADDR_DSPL
+    addi $t0, $t0, 16
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    sw $t1, 0($t0)
+    addi $t0, $t0, 124
+    sw $t1, 0($t0)
+    j game_loop
+    
 Exit:
 li $v0, 10 # terminate the program gracefully
 syscall
